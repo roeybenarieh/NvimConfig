7b53f347cbc2a66d7511e0af2537bbf8316399e4
roey ben arieh <roey280404@gmail.com>
2023-11-25 22:17:12 +0200

    feat(keymap): increase/decrease text font size

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+local write_err_to_user = vim.api.nvim_err_writeln
+
+-- increase/decrease text font size
+map("n", "<C-S-=>", function()
+  write_err_to_user("Increase text size by pressing Ctrl + Shift + =")
+end, { desc = "increase text/font size" })
+
+map("n", "<C-->", function()
+  write_err_to_user("decrease text size by pressing Ctrl + -")
+end, { desc = "decrease text/font size" })
