5e686889f9736b756706e965e2ef350348e4b305
roey ben arieh <roey280404@gmail.com>
2023-11-27 20:46:07 +0200

    feat(plugin): gitsigns- add line blame

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,14 +1,18 @@
 return {
-    "lewis6991/gitsigns.nvim",
-    event = "LazyFile",
-    opts = {
-        signs = {
-            add = { text = "+" },
-            change = { text = "~" },
-            delete = { text = "-" },
-            topdelete = { text = "-" },
-            changedelete = { text = "-" },
-            untracked = { text = "U" },
-        },
-    }
+  "lewis6991/gitsigns.nvim",
+  event = "LazyFile",
+  opts = {
+    signs = {
+      add = { text = "+" },
+      change = { text = "~" },
+      delete = { text = "-" },
+      topdelete = { text = "-" },
+      changedelete = { text = "-" },
+      untracked = { text = "U" },
+    },
+    current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+    current_line_blame_formatter_opts = {
+      relative_time = true, -- show relative time of the commit
+    },
+  },
 }
