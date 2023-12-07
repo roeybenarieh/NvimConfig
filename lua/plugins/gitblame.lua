5967cf3ede7b8b2c2be9dffe021cb3ae63269eac
roey ben arieh <roey280404@gmail.com>
2023-12-07 21:44:37 +0200

    feat(plugin): gitlab MR integration

--- lua/plugins/gitblame.lua
+++ lua/plugins/gitblame.lua
@@ -2,13 +2,13 @@ return {
   "f-person/git-blame.nvim",
   keys = {
     {
-      "<leader>gr",
+      "<leader>go",
       "<cmd> :GitBlameOpenFileURL <CR>",
       desc = "open in browser remote repo (github/gitlab)",
     },
   },
   opts = {
     enabled = true, -- this plugin is enabled only for the 'GitBlameOpenFileURL' keymap
-    message_template = "", -- show nothing in the git blame line
+    message_template = nil, -- show nothing in the git blame line
   },
 }
