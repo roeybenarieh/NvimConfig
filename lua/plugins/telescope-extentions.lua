1269918bfb9d74b9c856e5d81f90b6363b5fce5d
roey ben arieh <roey280404@gmail.com>
2023-12-08 16:43:06 +0200

    feat(plugin): "telescope-dap" telescope extension

---
+++ lua/plugins/telescope-extentions.lua
@@ -0,0 +1,11 @@
+return {
+  "telescope.nvim",
+  dependencies = {
+    {
+      "nvim-telescope/telescope-dap.nvim",
+      config = function()
+        require("telescope").load_extension("dap")
+      end,
+    },
+  },
+}
