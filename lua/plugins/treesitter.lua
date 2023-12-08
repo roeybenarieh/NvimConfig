22e2f9726be1be3c1015f91af8d3c29d2a9da968
roey ben arieh <roey280404@gmail.com>
2023-12-08 15:28:16 +0200

    feat(treesitter): more languages parsers

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,28 +1,35 @@
 return {
-    -- add more treesitter parsers
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-        ensure_installed = {
-            "vim",
-            "markdown_inline",
-            "lua",
-            "python",
-            "bash",
-            "go",
-            "markdown",
-            "rust",
-            "cpp",
-            "c_sharp",
-            "javascript",
-            "typescript",
-            "query",
-            "sql",
-            "promql",
-            "regex",
-            "html",
-            "css",
-            "json",
-            "yaml"
-        },
+  -- add more treesitter parsers
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "vim",
+      "markdown_inline",
+      "lua",
+      "python",
+      "bash",
+      "go",
+      "markdown",
+      "rust",
+      "cpp",
+      "c_sharp",
+      "javascript",
+      "typescript",
+      "query", -- for tree-sitter queries
+      "sql",
+      "promql",
+      "regex",
+      "html",
+      "css",
+      "json",
+      "json5",
+      "yaml",
+      "ini",
+      "git_config", -- .git/config file
+      "git_rebase", -- git interactive rebase file
+      "gitattributes", -- .gitattributes file, resource: https://git-scm.com/book/en/v2/Customizing-Git-Git-Attributes
+      "gitcommit", -- git commit message using the "conventional commits" specifications
+      "gitignore", -- .gitignore file
     },
+  },
 }
