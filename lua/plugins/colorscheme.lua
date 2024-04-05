83c92cf5513cf8adfa8a8459900bafcbf2856978
roey ben arieh <roey280404@gmail.com>
2024-04-05 22:51:18 +0300

    feat(colorscheme): more colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,21 @@
 return {
-    -- add gruvbox
-    { "ellisonleao/gruvbox.nvim" },
-  
-    -- Configure LazyVim to load gruvbox
-    {
-      "LazyVim/LazyVim",
-      opts = {
-        colorscheme = "gruvbox",
-      },
-    }
-  }
+  { "ellisonleao/gruvbox.nvim" },
+  -- vscode like theme
+  { "Mofiqul/vscode.nvim" },
+  -- IntelliJ like theme
+  { "briones-gabriel/darcula-solid.nvim", dependencies = "rktjmp/lush.nvim" },
+  -- catppuccino theme
+  { "catppuccin/nvim" },
+  -- github theme
+  { "projekt0n/github-nvim-theme" },
+  -- onedark theme
+  { "navarasu/onedark.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
