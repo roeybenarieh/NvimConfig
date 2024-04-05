da402ddaf5ca12c5d23e67689bd4d1bcbe521f48
roey ben arieh <roey280404@gmail.com>
2024-04-05 16:15:01 +0300

    feat(plugin): venv-selector automatic venv selection

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,13 +2,15 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- add hook to automatically select the venv when Nvim open
-local function on_attach(client)
-  -- select venv from cache via venv-selector.nvim
-  require("venv-selector").retrieve_from_cache()
-end
--- since lazyvim doesnt settup pyright, its fine to re-setup it.
-local lspconfig = require("lspconfig")
-lspconfig.pyright.setup({
-  on_attach = on_attach,
+-- activate python venv when Nvim open
+vim.api.nvim_create_autocmd("VimEnter", {
+  desc = "Auto select virtualenv Nvim open",
+  pattern = "*",
+  callback = function()
+    local venv = vim.fn.findfile("pyproject.toml", vim.fn.getcwd() .. ";")
+    if venv ~= "" then
+      require("venv-selector").retrieve_from_cache()
+    end
+  end,
+  once = true,
 })
