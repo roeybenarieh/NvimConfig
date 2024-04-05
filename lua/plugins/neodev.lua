02f2577888b16f19aa02665097d35d1b45d2d1e3
roey ben arieh <roey280404@gmail.com>
2024-04-05 23:01:34 +0300

    fix(plugin): remove incorrect neodev configuration

--- lua/plugins/neodev.lua
+++
@@ -1,9 +0,0 @@
-return {
-    -- signature help, docs and completion for the nvim lua API
-    "folke/neodev.nvim",
-    opts = {
-        library = {
-            plugins = { "nvim-treesitter", "plenary.nvim", "telescope.nvim" },
-        },
-    }
-}
