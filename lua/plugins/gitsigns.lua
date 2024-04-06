8ff2ea552265b31e2ae009258698aaf996298945
roey ben arieh <roey280404@gmail.com>
2024-04-06 12:33:16 +0300

    fix(keymap): correct gitsigns keymap names
    the <leader>gh showed 'hunk' instead of 'github'

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -24,18 +24,20 @@ return {
       -- stylua: ignore start
       local wk = require("which-key")
       wk.register({
-        ga = {
-          name = "+Action",
-          s = { mode = { "n", "v" }, ":Gitsigns stage_hunk<CR>", "Stage Hunk" },
-          r = { mode = { "n", "v" }, ":Gitsigns reset_hunk<CR>", "Reset Hunk" },
-          S = { gs.stage_buffer, "Stage Buffer" },
-          u = { mode = { "n", "v" }, gs.undo_stage_hunk, "Undo Stage Hunk" },
-          R = { gs.reset_buffer, "Reset Buffer" },
-          p = { mode = { "n", "v" }, gs.preview_hunk, "Preview Hunk" },
-          b = { function() gs.blame_line({ full = true }) end, "Blame Line" },
-          d = { mode = { "n", "v" }, gs.diffthis, "Diff This" },
-          D = { function() gs.diffthis("~") end, "Diff this ~" },
-        },
+        g = {
+          a = {
+            name = "+Action",
+            s = { mode = { "n", "v" }, ":Gitsigns stage_hunk<CR>", "Stage Hunk" },
+            r = { mode = { "n", "v" }, ":Gitsigns reset_hunk<CR>", "Reset Hunk" },
+            S = { gs.stage_buffer, "Stage Buffer" },
+            u = { mode = { "n", "v" }, gs.undo_stage_hunk, "Undo Stage Hunk" },
+            R = { gs.reset_buffer, "Reset Buffer" },
+            p = { mode = { "n", "v" }, gs.preview_hunk, "Preview Hunk" },
+            b = { function() gs.blame_line({ full = true }) end, "Blame Line" },
+            d = { mode = { "n", "v" }, gs.diffthis, "Diff This" },
+            D = { function() gs.diffthis("~") end, "Diff this ~" },
+          },
+        }
       }, { prefix = "<leader>" })
 
       map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
