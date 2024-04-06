05a54da92a756645cf493f99d4c1dfd48928043d
roey ben arieh <roey280404@gmail.com>
2024-04-06 23:07:46 +0300

    fix(plugin): venv-selector automatic venv selection
    PATCH: when the event "BufEnter" accures for the first time retriving venv from cache.
    for some reason the other events like "VimEnter" didnt work. this works for now.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -3,14 +3,11 @@
 -- Add any additional autocmds here
 
 -- activate python venv when Nvim open
-vim.api.nvim_create_autocmd("VimEnter", {
+vim.api.nvim_create_autocmd("BufEnter", {
   desc = "Auto select virtualenv Nvim open",
   pattern = "*",
   callback = function()
-    local venv = vim.fn.findfile("pyproject.toml", vim.fn.getcwd() .. ";")
-    if venv ~= "" then
-      require("venv-selector").retrieve_from_cache()
-    end
+    require("venv-selector").retrieve_from_cache()
   end,
   once = true,
 })
