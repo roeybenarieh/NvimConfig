3180d1495426b39237a0978e8944a95f2312dc80
roey ben arieh <roey280404@gmail.com>
2024-04-19 22:59:57 +0300

    fix(plugin): fix automatic venv selection
    fix an Issue accrued because the venv has been selected automatically before the LSP has been loaded, causing Pyright to think there is no venv

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -3,11 +3,15 @@
 -- Add any additional autocmds here
 
 -- activate python venv when Nvim open
-vim.api.nvim_create_autocmd("BufEnter", {
-  desc = "Auto select virtualenv Nvim open",
-  pattern = "*.py",
-  callback = function()
-    require("venv-selector").retrieve_from_cache()
+-- from issue: https://github.com/linux-cultist/venv-selector.nvim/issues/96#issuecomment-2010242590
+local augroup = vim.api.nvim_create_augroup("VenvSelectorRetrieve", { clear = true })
+vim.api.nvim_create_autocmd({ "LspAttach" }, {
+  pattern = { "*.py" },
+  group = augroup,
+  callback = function(args)
+    if vim.lsp.get_client_by_id(args["data"]["client_id"])["name"] == "pyright" then
+      require("venv-selector").retrieve_from_cache()
+      vim.api.nvim_del_augroup_by_id(augroup)
+    end
   end,
-  once = true,
 })
