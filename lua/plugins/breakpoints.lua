b6dd563f9bf9af4c5fa8b7ce667b29b141cf9868
roey ben arieh <roey280404@gmail.com>
2024-04-22 23:37:16 +0300

    feat(runtime): make persistent-breakpoints.nvim lazy load properly

--- lua/plugins/breakpoints.lua
+++ lua/plugins/breakpoints.lua
@@ -1,55 +1,51 @@
-local keymap = vim.api.nvim_set_keymap
+--[[
+IMPORTANT for future development:
+-nvim-dap must be loaded before persistent-breakpoints.nvim, otherwise there are errors.
+-persistent-breakpoints.nvim must be loaded before a file is opned, otherwise breakpoints are not restored.
+-if restoring an nvim session (using persistence.nvim) in the UI we see an open buffer, but the 'BufRead.*' event is not triggered, so breakpoints are not restored.
+currently since the breakpoints are anyway necessery only after LSP is attached, we use that event to load the breakpoints(which works even if restoring session).
+]]
 return {
-  "Weissle/persistent-breakpoints.nvim",
-  lazy = true,
-  config = function()
-    require("persistent-breakpoints").setup({
-      load_breakpoints_event = { "BufReadPost" },
-    })
+  {
+    "mfussenegger/nvim-dap",
 
-    keymap(
-      "n",
-      "<leader>db",
-      "<cmd>lua require('persistent-breakpoints.api').toggle_breakpoint()<cr>",
-      { noremap = true, silent = true, desc = "Toggle Breakpoint" }
-    )
-    keymap(
-      "n",
-      "<leader>dB",
-      [[:lua require("persistent-breakpoints.api").set_conditional_breakpoint()<CR>]],
-      { noremap = true, silent = true, desc = "Breakpoint Condition" }
-    )
-    keymap(
-      "n",
-      "<leader>dbc",
-      [[:lua require("persistent-breakpoints.api").clear_all_breakpoints()<CR>]],
-      { noremap = true, silent = true, desc = "Clear Breakpoints" }
-    )
-  end,
-  -- neither the normal 'keys = ...' works
-  -- keys = function()
-  --   return {
-  --     {
-  --       "<leader>db",
-  --       function()
-  --         require("persistent-breakpoints.api").toggle_breakpoint()
-  --       end,
-  --       "TToggle Breakpoint",
-  --     },
-  --     {
-  --       "<leader>dB",
-  --       function()
-  --         require("persistent-breakpoints.api").set_conditional_breakpoint()
-  --       end,
-  --       "BBreakpoint Condition",
-  --     },
-  --     {
-  --       "<leader>dbc",
-  --       function()
-  --         require("persistent-breakpoints.api").clear_all_breakpoints()
-  --       end,
-  --       "CClear BreakPoints",
-  --     },
-  --   }
-  -- end,
+    dependencies = {
+      "Weissle/persistent-breakpoints.nvim",
+    },
+  },
+  {
+    "Weissle/persistent-breakpoints.nvim",
+    opts = {
+      load_breakpoints_event = "LspAttach",
+    },
+    keys = {
+      {
+        "<leader>db",
+        function()
+          require("persistent-breakpoints.api").toggle_breakpoint()
+        end,
+        silent = true,
+        desc = "Toggle Breakpoint",
+        remap = false, -- override default mapping
+      },
+      {
+        "<leader>dB",
+        function()
+          require("persistent-breakpoints.api").set_conditional_breakpoint()
+        end,
+        silent = true,
+        desc = "Breakpoint Condition",
+        remap = false,
+      },
+      {
+        "<leader>dbc",
+        function()
+          require("persistent-breakpoints.api").clear_all_breakpoints()
+        end,
+        silent = true,
+        desc = "Clear Breakpoints",
+        remap = false,
+      },
+    },
+  },
 }
