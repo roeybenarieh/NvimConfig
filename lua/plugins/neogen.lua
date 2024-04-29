b16851c1ca8634acf3ad2780e5011effce85f8cc
roey ben arieh <roey280404@gmail.com>
2024-04-29 17:25:31 +0300

    fix(plugin): added cycling to Neogen docstrings
    used a snippet engine for handling the cycling after generating the skeleton of the docstring

--- lua/plugins/neogen.lua
+++ lua/plugins/neogen.lua
@@ -1,22 +1,26 @@
 return {
-    -- annotation generator
-    "danymat/neogen",
-    dependencies = "nvim-treesitter/nvim-treesitter",
-    config = true,
-    version = "*", -- follow only stable versions
-    keys = {
-        { "<leader>cds", "<cmd>:Neogen<cr><cmd>:+1<cr>", desc = "Create Docstring" },
+  -- annotation generator
+  "danymat/neogen",
+  dependencies = "nvim-treesitter/nvim-treesitter",
+  version = "*", -- follow only stable versions
+  keys = {
+    {
+      "<leader>cds",
+      function()
+        require("neogen").generate({})
+      end,
+      desc = "Create Docstring",
     },
-    opts = {
-        enabled = true,
-        insert_after_comment = true,
-        jump_map = "<C-x>", -- doesnt work, suppose to cycle threw all of the unfilled annotations
-        languages = {
-            python = {
-                template = {
-                    annotation_convention = "reST",
-                },
-            },
+  },
+  opts = {
+    enabled = true,
+    snippet_engine = "luasnip", -- integration with snippet engine, help with cycling support.
+    languages = {
+      python = {
+        template = {
+          annotation_convention = "reST",
         },
-    }
+      },
+    },
+  },
 }
