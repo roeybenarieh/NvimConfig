bdbc6f2e686ac94b16ae9fa0b42ee88f07307329
roey ben arieh <roey280404@gmail.com>
2024-05-03 14:31:19 +0300

    fix(plugin): change when dap-ui automatically closing

---
+++ lua/plugins/dap-ui.lua
@@ -0,0 +1,41 @@
+return {
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+    },
+    -- override lavyvim's config
+    -- for more information on the events dap has, see:
+    -- the possible events: https://microsoft.github.io/debug-adapter-protocol/specification#Events_Exited
+    -- the way to use them: https://github.com/mfussenegger/nvim-dap/blob/master/doc/dap.txt#L1182
+    config = function(_, opts)
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup(opts)
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open({})
+      end
+      -- lazy.nvim code I commented:
+      -- dap.listeners.before.event_terminated["dapui_config"] = function()
+      --   dapui.close({})
+      -- end
+      -- dap.listeners.before.event_exited["dapui_config"] = function()
+      --   dapui.close({})
+      -- end
+    end,
+    keys = {
+      {
+        "<leader>dt",
+        function()
+          require("dap").terminate()
+          require("dapui").close()
+        end,
+        desc = "Terminate",
+        -- non-recursive mapping
+        noremap = false,
+        -- silent meaning no message will be displayed
+        silent = true,
+      },
+    },
+  },
+}
