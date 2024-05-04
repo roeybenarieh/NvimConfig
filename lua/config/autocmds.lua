65ea14545cf5514d98d2b3c713eef44b0e5e59f6
roey ben arieh <roey280404@gmail.com>
2024-05-04 22:37:38 +0300

    feat(python): add global working directory to PYTHONPATH
    allow python (lsp and dap) to find modules from the root path of the project no matter where is it run from

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,6 +2,26 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+local function add_to_python_path(new_path)
+  local python_path = vim.fn.getenv("PYTHONPATH")
+  if python_path == vim.NIL then
+    python_path = ""
+  else
+    python_path = python_path .. ":"
+  end
+  python_path = python_path .. new_path
+  vim.fn.setenv("PYTHONPATH", python_path)
+end
+
+vim.api.nvim_create_autocmd({ "BufReadPre" }, {
+  pattern = { "*.py" },
+  callback = function()
+    local global_working_directory = vim.fn.getcwd(-1, -1)
+    add_to_python_path(global_working_directory)
+  end,
+  once = true,
+})
+
 -- activate python venv when Nvim open
 -- from issue: https://github.com/linux-cultist/venv-selector.nvim/issues/96#issuecomment-2010242590
 local augroup = vim.api.nvim_create_augroup("VenvSelectorRetrieve", { clear = true })
