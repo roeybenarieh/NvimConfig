6a04ad8b7576af921e394ff7362b8254066cd998
roey ben arieh <roey280404@gmail.com>
2024-06-08 16:59:56 +0300

    fix(plugin): remove nativly enabled venv-selector features

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -24,14 +24,14 @@ vim.api.nvim_create_autocmd({ "BufReadPre" }, {
 
 -- activate python venv when Nvim open
 -- from issue: https://github.com/linux-cultist/venv-selector.nvim/issues/96#issuecomment-2010242590
-local augroup = vim.api.nvim_create_augroup("VenvSelectorRetrieve", { clear = true })
-vim.api.nvim_create_autocmd({ "LspAttach" }, {
-  pattern = { "*.py" },
-  group = augroup,
-  callback = function(args)
-    if vim.lsp.get_client_by_id(args["data"]["client_id"])["name"] == "pyright" then
-      require("venv-selector").retrieve_from_cache()
-      vim.api.nvim_del_augroup_by_id(augroup)
-    end
-  end,
-})
+-- local augroup = vim.api.nvim_create_augroup("VenvSelectorRetrieve", { clear = true })
+-- vim.api.nvim_create_autocmd({ "LspAttach" }, {
+--   pattern = { "*.py" },
+--   group = augroup,
+--   callback = function(args)
+--     if vim.lsp.get_client_by_id(args["data"]["client_id"])["name"] == "pyright" then
+--       require("venv-selector").retrieve_from_cache()
+--       vim.api.nvim_del_augroup_by_id(augroup)
+--     end
+--   end,
+-- })
--- lua/plugins/venv-selctor.lua
+++ lua/plugins/venv-selctor.lua
@@ -4,10 +4,5 @@ return {
     opts = {
       parent = 0,
     },
-    -- hook, when venv is selected: create dap debug configurations
-    keys = {
-      -- Keymap to retrieve the venv from a cache
-      { "<leader>cc", "<cmd>:VenvSelectCached<cr>", desc = "venv fom cache" },
-    },
   },
 }
