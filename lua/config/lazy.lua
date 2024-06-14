964dc679658791d47f27f4b9525c2d608b8b3b1f
roey ben arieh <roey280404@gmail.com>
2024-06-14 13:36:50 +0300

    feat(leanguge): add git to lazyvim languages

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -58,6 +58,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.sql" },
     { import = "lazyvim.plugins.extras.lang.toml" },
 
+    { import = "lazyvim.plugins.extras.lang.git" },
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.terraform" },
