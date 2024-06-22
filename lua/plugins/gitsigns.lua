9a9e4814d6a310bbe2bd0c102dbda66b23189918
roey ben arieh <roey280404@gmail.com>
2024-06-22 16:46:02 +0300

    fix(plugin): remove deprecated gitsigns configuration option

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -11,9 +11,6 @@ return {
       untracked = { text = "U" },
     },
     current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
-    current_line_blame_formatter_opts = {
-      relative_time = true, -- show relative time of the commit
-    },
     on_attach = function(buffer)
       local gs = package.loaded.gitsigns
 
