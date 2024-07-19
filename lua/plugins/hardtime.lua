7ca72a8d9ba96b956f3e2f2b55eacd9835074b57
roey ben arieh <roey280404@gmail.com>
2024-07-19 14:32:34 +0300

    fix: allow mouse

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -2,6 +2,6 @@ return {
   "m4xshen/hardtime.nvim",
   dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
   opts = {
-    disable_mouse = true,
+    disable_mouse = false,
   },
 }
