f76512afb147c06d83a6adb13b73a6862e1cdefa
roey ben arieh <roey280404@gmail.com>
2024-08-10 16:16:47 +0300

    fix(plugin): restore breakpoints properly

--- lua/plugins/dap-virtual-text.lua
+++ lua/plugins/dap-virtual-text.lua
@@ -1,6 +1,7 @@
 return {
   "theHamsta/nvim-dap-virtual-text",
-  lazy = true,
+  -- for some reason this fucks up persistent-breakpoints.nvim
+  -- lazy = true,
   opts = {
     all_frames = true, -- shows virtual text for all frames that are in your file
   },
